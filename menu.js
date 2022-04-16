60130132b0c7d3b4e3a256248289505f5a52c48a
Lucas Murpf Dos Santos <lucasmurpf1@gmail.com>
2022-04-16 12:06:10 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,18 @@
+const btnmobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnmobile.addEventListener('click',toggleMenu);
+btnmobile.addEventListener('touchstart',toggleMenu);
+
